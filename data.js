435e0599cf98de5e0ba7d9d71f321a42b7d0bb74
Kurvaaa <93648377+bosniankicks@users.noreply.github.com>
2024-09-11 08:07:05 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,10 @@
+
+function challenge() {
+      var val = ([1]+[1]%1-1)+((!![]||!![])+!![]<<(true << 1))+7+(!!{}+!!{}+!!{}<<1)+5+(_ = 1, __ = 1 + 1, ___ = _ << __)+((1<<1)/(1<11) + true)+(true << 1)+(!{}|!!{})+(!{}|!!{})+(true << 1)+((1<<1)/(1<11) + true)+(_ = 1, __ = 1 + 1, ___ = _ << __)+5+(!!{}+!!{}+!!{}<<1)+7+((!![]||!![])+!![]<<(true << 1))+([1]+[1]%1-1)+36;
+      var tokenCookie = "__ecbmchid=j/jtS6tMJhf6MkVpM244E5CFMtMmI2VI0DiIgW/Rhn8pwNEcRs4QH2clZ9Rdw5+zxaYGj2anPQNbQWzQFb7E3pfQIxcyylBnusoYoHbrnVPmxhJHYIfq7WsxO1n5VzxLn8my8qQ5OfPaoQVgGpFXAiV6CLpdyvlrs1qq2EUejNh7XNWMDBsxHLP1q1EpXZHIHU/WWXx/d+kYCubYI8VmvdC7viDyokMbSR7rH9+jntqtcsadwj2fBnTPyzuy1IP+i5KkgoKIVHkUz1RhO5VT"+";path=/";
+      var answerCookie = "__eccha="+val+";path=/";
+console.log("Val: " + val);
+console.log("Token Cookie: " + tokenCookie);
+console.log("Answer Cookie: " + answerCookie);
+}  // Closing brace for the challenge function
+challenge();
